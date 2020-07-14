008593852b1c9916f93a93b2cd894c7e88d19682
sourav santra <souravsantra59580@gmail.com>
2020-07-14 22:49:47 +0530

    2nd commit(14/07/2020)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -35,7 +36,7 @@ void main(){
               child: Text("Camera"),
             ),
             Container(
-              child: Text("Chat"),
+              child: Chat(),
             ),
             Container(
               child: Text("Status"),
@@ -48,4 +49,83 @@ void main(){
       ),
     ),
   ));
+}
+
+
+class Chat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+        children: <Widget>[
+          InkWell(
+            child: ListTile(
+              leading: new CircleAvatar(
+                backgroundImage: NetworkImage("https://www.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg"),
+                radius: 25,
+              ),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text("Philip",style: TextStyle(fontWeight: FontWeight.bold),),
+                  Text("21:43 AM",style: new TextStyle(color: Color(0xff39D770), fontSize: 14.0),),
+                ],
+              ),
+              subtitle: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text("Please tell me your full name"),
+                  CircleAvatar(
+                    backgroundColor: Color(0xff39D770),
+                    radius: 15,
+                    child: Text('20',style: TextStyle(color: Colors.white),),
+                  )
+                ],
+              ),
+            ),
+            onTap: (){},
+          ),
+          Divider(
+            indent: 70,
+            endIndent: 10,
+            color: Colors.grey,
+          ),
+          InkWell(
+            child: ListTile(
+              leading: new CircleAvatar(
+                backgroundImage: NetworkImage("https://m.cricbuzz.com/a/img/v1/192x192/i1/c170661/virat-kohli.jpg"),
+                radius: 25,
+              ),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text("Virat Kholi",style: TextStyle(fontWeight: FontWeight.bold),),
+                  Text("21:43 AM",style: new TextStyle(color: Color(0xff39D770), fontSize: 14.0),),
+                ],
+              ),
+              subtitle: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text("Please tell me your full name"),
+                  CircleAvatar(
+                    backgroundColor: Color(0xff39D770),
+                    radius: 15,
+                    child: Text('20',style: TextStyle(color: Colors.white),),
+                  )
+                ],
+              ),
+            ),
+            onTap: (){},
+          ),
+          Divider(
+            indent: 70,
+            endIndent: 10,
+            color: Colors.grey,
+          ),
+
+        ],
+      ),
+
+    );
+  }
 }
